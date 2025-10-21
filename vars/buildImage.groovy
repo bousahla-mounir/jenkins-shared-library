24caa647ca809b40f02a3e6e52c7409ecc59ebbe
hilal13 <hilal@gmail.com>
2025-10-21 17:15:57 +0100

    debugging docker image v2

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building the docker image..."
-    /*withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t adabachir/demo-repo:jma-6.31 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanajanashia/demo-app:jma-2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push adabachir/demo-repo:jma-6.31'
-    }*/
+        sh 'docker push nanajanashia/demo-app:jma-2.1'
+    }
 }
