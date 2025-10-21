8d38eeb529e3cae3d6c6e612da3d6f14f8ff45af
hilal13 <hilal@gmail.com>
2025-10-21 16:58:52 +0100

    change tag of remote docker image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t adabachir/demo-repo:jma-2.2 .'
+        sh 'docker build -t adabachir/demo-repo:jma-6.31 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push adabachir/demo-repo:jma-2.2'
     }
