32e09ab663c3d66df0a87a4f75bdbb0b23d19524
hilal13 <hilal@gmail.com>
2025-10-21 17:54:44 +0100

    call docker build with param

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t adabachir/demo-repo:jma-6.9 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push adabachir/demo-repo:jma-6.9'
+        sh "docker push $imageName"
     }
 }
